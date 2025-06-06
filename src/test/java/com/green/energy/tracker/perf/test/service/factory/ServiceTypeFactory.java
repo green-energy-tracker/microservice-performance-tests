9a3e938aeda6b7314d7bae73d2b95bd518c50f9a
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-06-06 14:24:09 +0200

    add site sensor perf test

--- src/test/java/com/green/energy/tracker/perf/test/service/factory/ServiceTypeFactory.java
+++ src/test/java/com/green/energy/tracker/perf/test/service/factory/ServiceTypeFactory.java
@@ -1,6 +1,5 @@
 package com.green.energy.tracker.perf.test.service.factory;
 
-import com.green.energy.tracker.perf.test.service.AllService;
 import com.green.energy.tracker.perf.test.service.Service;
 import com.green.energy.tracker.perf.test.service.SiteSensorManagement;
 import com.green.energy.tracker.perf.test.service.UserManagement;
@@ -16,7 +15,6 @@ public final class ServiceTypeFactory {
         switch (service) {
             case USER_MANAGEMENT: serviceTypeFactory = UserManagement.builder().service(service).build(); break;
             case SITE_SENSOR_MANAGEMENT: serviceTypeFactory = SiteSensorManagement.builder().service(service).build(); break;
-            case ALL: serviceTypeFactory = AllService.builder().service(service).build(); break;
         }
         return serviceTypeFactory;
     }
